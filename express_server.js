7c104af2e87587507d8e30c84aa3d88f111cb0f7
moe <mbchehade@gmail.com>
2020-01-20 20:48:59 +0000

    set the Express app to use EJS as its templating engine

--- express_server.js
+++ express_server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.set("view engine", "ejs");//This tells the Express app to use EJS as its templating engine
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -31,3 +32,4 @@ app.get("/set", (req, res) => {
 app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
 });
+
