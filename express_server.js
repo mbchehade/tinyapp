21d7f5da8a99ba36bace29970bdba10987ce74da
moe <mbchehade@gmail.com>
2020-01-20 22:24:05 +0000

    rended shortURL and longURL and passed it to templateVars object

--- express_server.js
+++ express_server.js
@@ -42,4 +42,7 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  res.render("urls_show", templateVars);
+});
