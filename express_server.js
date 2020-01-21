b1d334f4a346ef1a2d7a5eb5d91f518073923bee
moe <mbchehade@gmail.com>
2020-01-21 02:09:06 +0000

    redirect longURLs and shortURLs

--- express_server.js
+++ express_server.js
@@ -51,14 +51,23 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  const str = generateRandomString()
+  urlDatabase[str] = (req.body)["longURL"];  
+  res.redirect(`/urls/${str}`)      
 });
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params['shortURL']];
+  res.redirect(`http://${longURL}`);
+});
+
+
     function generateRandomString() {
       const length = 6;
       return Math.round((Math.pow(36, length + 1) - Math.random() * Math.pow(36, length))).toString(36).slice(1);
