5c4ad7790a0b756c1404cc55319b94b36d49bc96
moe <mbchehade@gmail.com>
2020-01-21 17:24:33 +0000

    added a route method post to delete URL

--- express_server.js
+++ express_server.js
@@ -5,11 +5,13 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-app.set("view engine", "ejs");//This tells the Express app to use EJS as its templating engine
+app.set("view engine", "ejs");//This tells the Express app to use EJS as its templating engine so view engine is the header and ejs is the value;
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+//this is an object called urlDatabase which has 2 properties which are keys and values. the keys are short urls and the values are long urls.
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -21,11 +23,11 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`Example app listening on port ${PORT}!`);// this is to get the server listening on a port.
 });
 
 app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+  res.json(urlDatabase); // this function is a GET request to get the object data in a json package. json package is used to trasmit data between server and web application.
 });
 
 app.get("/hello", (req, res) => {
@@ -35,7 +37,7 @@ app.get("/hello", (req, res) => {
 app.get("/set", (req, res) => {
   const a = 1;
   res.send(`a = ${a}`);
-});
+}); //it is a GET method which requests a representation of the specified resource. this only retrives data.
 
 app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
@@ -43,12 +45,14 @@ app.get("/fetch", (req, res) => {
 
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
+  console.log("templateVars", templateVars)
   res.render("urls_index", templateVars);
-});
+}); // this route will render the urlDatabase and show it on the page from the template on urlindex file
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
-});
+}); // this goes to the new path and render template in urlnew.
+
 
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
@@ -56,7 +60,7 @@ app.get("/urls/:shortURL", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
+  console.log(req.body);  
   const str = generateRandomString()
   urlDatabase[str] = (req.body)["longURL"];  
   res.redirect(`/urls/${str}`)      
@@ -67,8 +71,13 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(`http://${longURL}`);
 });
 
+app.post("/urls/:shortURL/delete", (req, res) =>{
+  delete urlDatabase[req.params.shortURL]
+  res.redirect("/urls")
+})
 
-    function generateRandomString() {
-      const length = 6;
+
+function generateRandomString() {
+  const length = 6;
       return Math.round((Math.pow(36, length + 1) - Math.random() * Math.pow(36, length))).toString(36).slice(1);
     }
